10e4290b6ac3579cb48d69ee8c3e38c0e6b89de8
bilel <bilel.belghith123@gmail.com>
2020-09-11 03:57:09 +0200

    v1.0.2

--- routes/web.php
+++ routes/web.php
@@ -15,11 +15,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-	//Route::get('/', ['as' => 'frontend.home', 'uses' => 'frontendController@home']); 
-	Route::get('about-us', ['as' => 'frontend.about-us', 'uses' => 'frontendController@about']);
-	Route::get('services', ['as' => 'frontend.services', 'uses' => 'frontendController@services']);
-	Route::get('portfolio', ['as' => 'frontend.portfolio', 'uses' => 'frontendController@portfolio']);
-	Route::get('elements', ['as' => 'frontend.elements', 'uses' => 'frontendController@elements']);
-	Route::get('blog', ['as' => 'frontend.blog', 'uses' => 'frontendController@blog']);
-	Route::get('blog-detail', ['as' => 'frontend.blog-detail', 'uses' => 'frontendController@blogDetail']);
-	Route::get('contact', ['as' => 'frontend.contact', 'uses' => 'frontendController@contact']);
+	
